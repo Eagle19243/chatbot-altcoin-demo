ac440ee63eb18eb1957695b457bd4be48ec88e9c
Eagle19243 <eagle19243@gmail.com>
2018-06-12 21:21:26 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="referer" content="no-referer">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+        <title>Chatbot-Altcoin</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <iframe
+            class="support-bot"
+            src="https://chatbot-altcoin.herokuapp.com/">
+        </iframe>
+    </body>
+</html>
